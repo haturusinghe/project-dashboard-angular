e1d5420650fd8e53ef1be2a4f9d70c7fb51ecb49
haturusinghe <s.haturusinghe99@gmail.com>
2023-11-06 12:54:26 +0530

    fix styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Project Management Dashboard';
+
+  showProjectList: boolean = true;
+  buttonTitle: string = "Go to Dashboard"
+
+  toggleView(): void {
+    this.showProjectList = !this.showProjectList;
+    if(this.showProjectList){
+      this.buttonTitle = "Go to Dashboard"
+    }else{
+      this.buttonTitle = "Go to Project List"
+    }
+  }
 }
