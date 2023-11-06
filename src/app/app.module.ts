1b9856571112e14599bccc9805c99bc053c83647
haturusinghe <s.haturusinghe99@gmail.com>
2023-11-06 12:29:51 +0530

    added 2 new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,17 @@ import { AppComponent } from './app.component';
 import { ProjectListComponent } from './components/project-list/project-list.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { TopBarComponent } from './components/top-bar/top-bar.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SaveProjectComponent } from './components/save-project/save-project.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProjectListComponent,
     SidebarComponent,
-    TopBarComponent
+    TopBarComponent,
+    DashboardComponent,
+    SaveProjectComponent
   ],
   imports: [
     HttpClientModule,
---
+++ src/app/components/save-project/save-project.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-save-project',
+  templateUrl: './save-project.component.html',
+  styleUrls: ['./save-project.component.scss']
+})
+export class SaveProjectComponent {
+
+}
