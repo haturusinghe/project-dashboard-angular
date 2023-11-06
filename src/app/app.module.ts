5991301ca4dfd281a56c0c1bbd102c6b3c66a562
haturusinghe <s.haturusinghe99@gmail.com>
2023-11-06 10:34:00 +0530

    added sidebar and topbar components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProjectListComponent } from './components/project-list/project-list.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProjectListComponent
+    ProjectListComponent,
+    SidebarComponent,
+    TopBarComponent
   ],
   imports: [
     BrowserModule,
