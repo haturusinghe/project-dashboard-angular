091ac1a6da218b83059f9019cef61c3312df6ff2
haturusinghe <s.haturusinghe99@gmail.com>
2023-11-07 04:59:22 +0530

    routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ProjectListComponent } from './components/project-list/project-list.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SaveProjectComponent } from './components/save-project/save-project.component';
+
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'list', component: ProjectListComponent },
+  { path: 'save', component: SaveProjectComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }, 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,12 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProjectListComponent } from './components/project-list/project-list.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { TopBarComponent } from './components/top-bar/top-bar.component';
+
+import { ProjectListComponent } from './components/project-list/project-list.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { SaveProjectComponent } from './components/save-project/save-project.component';
-import { AddProjectComponent } from './components/add-project/add-project.component';
 
 import { FormsModule } from '@angular/forms';
 
@@ -21,15 +21,9 @@ import { FormsModule } from '@angular/forms';
     TopBarComponent,
     DashboardComponent,
     SaveProjectComponent,
-    AddProjectComponent
   ],
-  imports: [
-    HttpClientModule,
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule 
-  ],
+  imports: [HttpClientModule, BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
