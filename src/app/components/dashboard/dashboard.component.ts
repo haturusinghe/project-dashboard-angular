19cb986d5b5059e017acf8ade5f12047a7495b94
haturusinghe <s.haturusinghe99@gmail.com>
2023-11-07 06:23:47 +0530

    show alert for completed projectrs

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,29 +1,34 @@
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Project } from 'src/app/models/project.model';
 import { ProjectService } from 'src/app/services/project/project.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
   topProjects: Project[] = [];
 
-  constructor(private projectService: ProjectService) { 
-  }
+  constructor(private projectService: ProjectService) {}
 
   ngOnInit(): void {
-    this.projectService.getTopProjectsByRevenue(3).subscribe(data => {
-      this.topProjects = data
+    this.projectService.getTopProjectsByRevenue(3).subscribe((data) => {
+      this.topProjects = data;
       console.log(this.topProjects);
     });
 
-    this.projectService.getCompletedProjects().subscribe(data => {
-      console.log("Completed", data);
+    this.projectService.getCompletedProjects().subscribe((data) => {
+      this.alertCompletedProjects(data);
     });
-
-    
   }
 
+  alertCompletedProjects(projects: Project[]): void {
+    let message = `Completed Projects: ${projects.length} \n \n`;
+    projects.forEach((project) => {
+      message += `\t Project: ${project.name} \n`;
+    });
+
+    alert(message);
+  }
 }
