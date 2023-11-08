3d63738c6b6a6931aba074bad4608ff10b879c84
haturusinghe <s.haturusinghe99@gmail.com>
2023-11-08 10:11:19 +0530

    refactor code by adding makeProjectList function

--- src/app/services/project/project.service.ts
+++ src/app/services/project/project.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { filter, map } from 'rxjs/operators';
 import { Project } from 'src/app/models/project.model';
 
 @Injectable({
@@ -15,62 +15,43 @@ export class ProjectService {
   getProjects(): Observable<Project[]> {
     const endpoint = 'projects/all';
     return this.http.get<any[]>(this.apiUrl + endpoint).pipe(
-      map((projectsData: any[]) => {
-        return projectsData.map(
-          (project) =>
-            new Project(
-              project.id,
-              project.name,
-              project.revenue,
-              project.isCompleted
-            )
-        );
-      })
+      map(this.makeProjectList)
     );
   }
+  
+  getTopProjectsByRevenue(count: number = 3): Observable<Project[]> {
+    const endpoint = 'projects/top';
+    return this.http.get<any[]>(this.apiUrl + endpoint).pipe(
+      map(this.makeProjectList)
+    );
+  }
+  
+  getCompletedProjects(): Observable<Project[]> {
+    const endpoint = 'projects/completed';
+    return this.http.get<any[]>(this.apiUrl + endpoint).pipe(
+      map(this.makeProjectList)
+    );
+  }
+  
 
   deleteProject(id: number): Observable<string> {
     const endpoint = 'projects/delete';
     return this.http.delete<string>(this.apiUrl + endpoint + '/' + id);
   }
 
-  getTopProjectsByRevenue(count: number = 3): Observable<Project[]> {
-    const endpoint = 'projects/top';
-    return this.http.get<any[]>(this.apiUrl + endpoint).pipe(
-      map((projectsData: any[]) => {
-        return projectsData.map(
-          (project) =>
-            new Project(
-              project.id,
-              project.name,
-              project.revenue,
-              project.isCompleted
-            )
-        );
-      })
-    );
-  }
-
-  getCompletedProjects(): Observable<Project[]> {
-    const endpoint = 'projects/completed';
-    return this.http.get<any[]>(this.apiUrl + endpoint).pipe(
-      map((projectsData: any[]) => {
-        return projectsData.map(
-          (project) =>
-            new Project(
-              project.id,
-              project.name,
-              project.revenue,
-              project.isCompleted
-            )
-        );
-      })
-    );
-  }
 
   addProject(project: Project): Observable<Project> {
     console.log(project);
     const endpoint = 'projects/save';
     return this.http.post<Project>(this.apiUrl + endpoint, project);
+  }
+
+  private makeProjectList(projectsData: any[]): Project[] {
+    return projectsData.map(project => new Project(
+      project.id,
+      project.name,
+      project.revenue,
+      project.isCompleted
+    ));
   }
 }
