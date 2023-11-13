99ebffd8a5ac4338dc153612f4b2d9df72f777be
haturusinghe <s.haturusinghe99@gmail.com>
2023-11-13 12:39:39 +0530

    minor change

--- src/app/services/project/project.service.ts
+++ src/app/services/project/project.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import {  map } from 'rxjs/operators';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 import { Project } from 'src/app/models/project.model';
 
 @Injectable({
@@ -15,30 +15,28 @@ export class ProjectService {
   getProjects(): Observable<Project[]> {
     const endpoint = 'projects/all';
     return this.http.get<any[]>(this.apiUrl + endpoint).pipe(
-      map(this.makeProjectList)
+      map(this.makeProjectList),
     );
   }
   
   getTopProjectsByRevenue(count: number = 3): Observable<Project[]> {
     const endpoint = 'projects/top/';
     return this.http.get<any[]>(this.apiUrl + endpoint + count).pipe(
-      map(this.makeProjectList)
+      map(this.makeProjectList),
     );
   }
   
   getCompletedProjects(): Observable<Project[]> {
     const endpoint = 'projects/completed';
     return this.http.get<any[]>(this.apiUrl + endpoint).pipe(
-      map(this.makeProjectList)
+      map(this.makeProjectList),
     );
   }
-  
 
   deleteProject(id: number): Observable<string> {
     const endpoint = 'projects/delete';
     return this.http.delete<string>(this.apiUrl + endpoint + '/' + id);
   }
-
 
   addProject(project: Project): Observable<Project> {
     console.log(project);
