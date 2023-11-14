f14991873c82b3cd39f0992e2e20d5652085855b
haturusinghe <s.haturusinghe99@gmail.com>
2023-11-14 16:54:54 +0530

    added error handeling for requests in all 3 components

--- src/app/components/save-project/save-project.component.ts
+++ src/app/components/save-project/save-project.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { ProjectService } from 'src/app/services/project/project.service';
-import { Project } from 'src/app/models/project.model'
+import { Project } from 'src/app/models/project.model';
 
 import { Router } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-save-project',
@@ -12,28 +13,24 @@ import { Router } from '@angular/router';
 export class SaveProjectComponent {
   newProject: Project;
 
-  constructor(private projectService: ProjectService, private router: Router) { 
-    this.newProject = new Project()
+  constructor(private projectService: ProjectService, private router: Router) {
+    this.newProject = new Project();
   }
 
-  
-
   saveProject() {
-
-    if(this.newProject.name.length > 0){
-      
-      this.projectService.addProject(this.newProject).subscribe((res) => {
-        
-        this.newProject = new Project()
-
-        console.log(res)
-        alert(res)
-        this.router.navigate(['/list']);
+    if (this.newProject.name.length > 0) {
+      this.projectService.addProject(this.newProject).subscribe({
+        next: (data) => {
+          alert(`Project saved successfully!,${data}`);
+          this.newProject = new Project();
+          this.router.navigate(['/list']);
+        },
+        error: (error: HttpErrorResponse) => {
+          alert(error.message);
+        },
       });
-    }else{
-      alert("Project Name cannot be empty")
+    } else {
+      alert('Project Name cannot be empty');
     }
-
-    
   }
 }
